cccf384971d910d3c79b77df6b7a531ed93ebe96
Unknown <dragonsheep1031@gmail.com>
2018-11-28 23:41:47 +0800

    First completed
    完成了初次的計算功能

--- src/App.js
+++ src/App.js
@@ -14,15 +14,12 @@ class App extends Component {
         // netWorkClass: 'Any',
         ipAddress: '0.0.0.0',
         responsive : { type: 'null', responsive: null},
-        data: { exp: 0, online: 0}
+        data: { exp: 0, online: 0},
+        countData: { index: null , data: []}
     };
   }
   componentDidUpdate(prevProps){
-    // if(this.props !== prevProps){
-    //   this.fetchData(this.props);
-    // }
-    console.log(this.state.data)
-    // this.state.data
+    // console.log(this.state.data)
   }
   stateSubNet = (val) =>{
     this.setState({maskIPVal: val})
@@ -30,31 +27,29 @@ class App extends Component {
   stateIpAddress = (val) =>{
     this.setState({ipAddress: val})
   }
-  // stateOnline = (val) =>{
-  //   setState={this.stateOnline}
-  // }
   render() {
-    const subnet = ['255','254','252','248','240','224','192','128','0'];
+    const subnet = ['255','254','252','248','240','224','192','128','0']
+    const reSubnet = ['0','128','192','224','240','248','252','254']
     let resetResponsive = () =>{
       this.setState({responsive: { type: 'null', responsive: null},data:{ exp: 0, online: 0}})
+    }
+    let saveResponsive = ( isType, textResponsive) =>{
+      this.setState({responsive: { 
+        type: isType,
+        responsive: textResponsive
+      }})
+
     }
     let justDoit = (e) =>{
       resetResponsive()
       let mask = this.state.maskIPVal.split('.')
       mask.forEach((val,index)=>{
         if(subnet.indexOf(val) === -1){
-          this.setState({responsive: { 
-            type: 'error',
-            responsive: '網路遮罩格式錯誤，請依照指定計算數值填寫'
-          }})
-
+          saveResponsive('error','網路遮罩格式錯誤，請依照指定計算數值填寫')
         }
         
         if(index > 3 && index < 0){
-          this.setState({responsive: { 
-            type: 'error',
-            responsive: '網路遮罩格式錯誤，請依照指定數值填寫長度'
-          }})
+          saveResponsive('error','網路遮罩格式錯誤，請依照指定數值填寫長度')
         }
        
         this.setState(prevState => ({
@@ -64,13 +59,77 @@ class App extends Component {
           }
         }));
       })
+
       this.setState(prevState => ({
         data:{
           exp:  prevState.data.exp,
           online: (Math.pow(2, prevState.data.exp) - 2) < 0 ? 0 : (Math.pow(2, prevState.data.exp) - 2)
         }
-      }));
-      
+      }),()=>{
+        outdata(mask);
+      });
+     
+    }
+    let outdata = (mask) => {
+      let ip = this.state.ipAddress.split('.')
+      let tempData = { index: null , data: []};
+      let isRepeat = false
+      let casePrint = (max, val) => {
+        tempData['index'] = val
+        let expNumber = Math.pow(2, this.state.data.exp % 8)
+        for(let i = 0; i < max ; i++){
+          let iExpNumber = i * expNumber;
+          let iExpNumberS = (i + 1) * expNumber - 1;
+          let startVal = `${val === 0 ? iExpNumber : ip[0]}.` +
+          `${val === 1 ? iExpNumber : val > 1 ? ip[1]: 0}.` +
+          `${val === 2 ? iExpNumber : val > 2 ? ip[2]: 0}.` +
+          `${val === 3 ? iExpNumber : 0}`
+          let endVal = `${val === 0 ? iExpNumberS : ip[0]}.` +
+          `${val === 1 ? iExpNumberS : val > 1 ? ip[1]: 255}.` +
+          `${val === 2 ? iExpNumberS : val > 2 ? ip[2]: 255}.` +
+          `${val === 3 ? iExpNumberS : 255}`
+
+          tempData['data'].push(startVal)
+          tempData['data'].push(endVal)
+        }
+        saveData()
+      }
+      let casePrints = ( val) => {
+        tempData['index'] = val
+        let startVal = `${ val === 0 ? 0 : ip[0]}`
+        let endVal = `${val === 0 ? 255 : ip[1]}`
+        for(let i = 1 ; i < 4 ; i++){
+          startVal +=  `.${ i > val ? 0 : ip[i]}`
+          endVal +=  `.${ i > val ? 255 : ip[i]}`
+        }
+        tempData['data'].push(startVal)
+        tempData['data'].push(endVal)
+        saveData()
+      }
+      let saveData = () =>{
+        this.setState({
+          countData : tempData
+        })
+      }
+      mask.forEach((val,index)=>{
+          if(reSubnet.indexOf(val) !== 0){
+            let max = Math.pow(2, reSubnet.indexOf(val))
+              if(val !== '255'){
+                
+                  casePrint(max, index)
+                
+                }
+              else{
+                if(!isRepeat){
+                  if(val === '255' && mask[(index + 1) < 4 ? (index + 1) : 3] === '0'){
+                    casePrints(index)  
+                  }
+                }
+                
+              }
+           }
+      })
+      //
     }
     return (
       <div className="App" key="App">
--- src/components/reply.js
+++ src/components/reply.js
@@ -7,15 +7,42 @@ class reply extends Component {
     let responsive = ''
     let onLine = ''
     if(setState.responsive.type !== 'null')
-      type = <span>{setState.responsive.type}</span>
+      type = <div>{setState.responsive.type}</div>
     if(setState.responsive !== null)
-      responsive = <span>{setState.responsive.responsive}</span>
-    onLine = <span>可使用的IP數量{setState.data.online}個</span>
+      responsive = <div>{setState.responsive.responsive}</div>
+    onLine = <div>可使用的IP數量{setState.data.online}個</div>
+    
+    let max = setState.countData.data.length
+    let data = Object.values(setState.countData.data)
+    let div = []
+    for(let i = 0; i < max ; i+=2){
+      let temp = data[i].split('.')
+      let temps = data[i + 1].split('.')
+      temp[setState.countData.index] = temp[setState.countData.index] * 1 + 1
+      temps[setState.countData.index] =temps[setState.countData.index] * 1 - 1
+      temp = temp.join('.')
+      temps = temps.join('.')
+      div.push(
+        <div className="tr" key={i.toString()}>
+          <span>{data[i]}</span>
+          <span>{temp} ~ {temps}</span>
+          <span>{data[i+1]}</span>
+        </div>
+      )
+    }
     return (
         <div className="components" key="reply">
           {type}
           {responsive}
           {onLine}
+          <div className="tr">
+            <span>Network Address</span>
+            <span>Usable Host Rang</span>
+            <span>Broadcast Address</span>
+          </div>
+          <div className="table">
+            {div}
+          </div>
         </div>    
       )
   }
